734938b53ea1b8f203384c8d082dd2a5bdcfe050
Ben Ong <jgv115@gmail.com>
2018-04-04 15:50:18 +1000

    added model loader

--- src/model_loader.py
+++ src/model_loader.py
@@ -1,1 +1,14 @@
 import keras
+
+
+class model_methods:
+    def __init__(self):
+        self.deepdive_model = keras.models.load_model('model1.h5')
+
+    def get_prediction(self, input):
+        # This method takes in an input of an array of strings and outputs a prediction
+        test = self.deepdive_model.predict(input)
+        print(test)
+
+test = model_methods()
+test
